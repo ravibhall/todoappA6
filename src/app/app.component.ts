382e45b258d01edbf4f380da3b9cee44ad1d6be1
ravibhall <ravibhall2010@gmail.com>
2018-08-31 16:35:07 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './css/font-awesome-4.7.0/css/font-awesome.min.css']
 })
-export class AppComponent {
-  title = 'demoApp';
+export class AppComponent implements OnInit {
+  cookieValue: any;
+  tasks: Taskclass[];
+  newTask: string;
+  forWhat: string;
+
+  constructor(private cookieService: CookieService) { }
+
+  ngOnInit(): void {
+    this.forWhat = "true";
+    this.cookieValue = this.cookieService.get('storedDataList');
+    if (this.cookieValue.length) {
+      this.tasks = JSON.parse(this.cookieValue);
+    } else {
+      this.tasks = [];
+    }
+    window.setTimeout(function () {
+    var inBox = document.getElementById("task-input");
+    inBox.focus();
+    },250);
+  }
+
+  editTask(task, i) {
+    task.edit = !task.edit;
+    task.editedTask = task.text;
+    window.setTimeout(function () {
+      var editField = document.getElementById("input-" + i);
+      editField.focus();
+    }, 250);
+
+  };
+  cancelTask(task) {
+    task.edit = !task.edit;
+  };
+  deleteTask(task) {
+    var ind;
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (task.id === this.tasks[i].id) {
+        ind = i;
+        break;
+      }
+    }
+    this.tasks.splice(ind, 1);
+    this.cookieService.set('storedDataList', JSON.stringify(this.tasks));
+  };
+  addTask(task) {
+    var obj = {
+      "text": task,
+      "id": this.tasks.length,
+      "edit": false
+    }
+    this.tasks.push(obj);
+    this.cookieService.set('storedDataList', JSON.stringify(this.tasks));
+    this.newTask = '';
+    var inBox = document.getElementById("task-input");
+    inBox.focus();
+  };
+  saveTask(task, index) {
+    this.tasks[index].text = task.editedTask;
+    task.edit = !task.edit;
+    this.cookieService.set('storedDataList', JSON.stringify(this.tasks));
+  }
+}
+export class Taskclass {
+  text: any;
+  id: number;
+  edit: boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CookieService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
